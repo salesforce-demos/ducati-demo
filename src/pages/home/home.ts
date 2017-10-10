bad37a155266fdacb32c418d9b5cabac28523f8b
Gabriel Rilo <gabriel@nimacloud.com>
2017-10-10 17:02:19 -0300

    Updating to Ionic 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { Socket } from 'ng-socket-io';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private socket: Socket, public toastCtrl: ToastController) {
 
+    this.socket.on('message', (data) => {
+
+      console.log(data);
+
+      const toast = this.toastCtrl.create({
+        message: data.message,
+        duration: 10000,
+        position: 'top'
+      });
+
+      toast.present();
+    });
   }
 
 }
